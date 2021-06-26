5b8d50129ba1b806681078e04cea2ba603e48a9d
vamsireddy22 <obulreddy1999@gmail.com>
2021-06-26 18:36:20 +0530

    Created Tables

--- Banking.py
+++ Banking.py
@@ -1,8 +1,14 @@
 import mysql.connector as mysql
 
-db_connection = mysql.connect(host='localhost',username='root',passwd='')
+db_connection = mysql.connect(host='localhost',username='root',passwd='',database='Banking_System')
 
-if(db_connection.is_connected()):
-    print("Connection Successfull")
-else:
-    print("Connection Failed")
+mycursor = db_connection.cursor()
+
+#mycursor.execute("create database Banking_System")
+mycursor.execute("create table Account(AccNum Integer,AccHolderName varchar(20),Amount integer,TypeOfAcc varchar(20), phno integer)")
+mycursor.execute("create table Amount(AccNum integer, Amount Integer)")
+
+print("Table created Successfully")
+
+
+
